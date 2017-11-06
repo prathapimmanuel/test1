3ecd03f5d9ec8e59be8f977e0c09a9bfdb020ddf
K <235222@cognizant.com>
2017-11-06 18:12:16 +0530

    commit latest

--- src/test/java/Test/GoogleTesting.java
+++ src/test/java/Test/GoogleTesting.java
@@ -1,28 +1,61 @@
 package Test;
 
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterTest;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import framework.core.TestNGBase;
+import framework.core.EnvParameters;
+import framework.core.EnvParameters.OSType;
+
+import java.io.File;
+import java.lang.reflect.Method;
 
 import org.junit.After;
-import org.testng.Assert;
-import org.testng.annotations.Test;
-import org.testng.asserts.SoftAssert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 import Pages.GooglePage;
-import framework.core.TestNGBase;
+
 
 /**
  * Unit test for simple App.
  */
-public class GoogleTesting extends TestNGBase {
-
+public class GoogleTesting  {
+	WebDriver driver;
 	String dataURL = "http://www.google.com";
 
 	//SoftAssert softAssert = getSoftAssert();
 	Boolean flag = false;
+	
+	
+	@BeforeTest
+private void setupChromeDriver() throws Exception {
+
+			String ChromProp = "webdriver.chrome.driver";
+			new EnvParameters();
+			File targetChromedriver = null;
+			if (EnvParameters.getOSname() == EnvParameters.OSType.windows) {			
+				targetChromedriver = new File(EnvParameters.TEST_ROOT_DIR
+						+ File.separator + "drivers" + File.separator + "chrome"
+						+ File.separator + "win" + File.separator
+						+ "chromedriver.exe");
+			}
+
+			else if (EnvParameters.getOSname() == OSType.mac) {
+				targetChromedriver = new File(EnvParameters.TEST_ROOT_DIR
+						+ File.separator + "drivers" + File.separator + "chrome"
+						+ File.separator + "mac" + File.separator + "chromedriver");
+			} else if (EnvParameters.getOSname() == OSType.linux) {
+				targetChromedriver = new File(EnvParameters.TEST_ROOT_DIR
+						+ File.separator + "drivers" + File.separator + "chrome"
+						+ File.separator + "linux" + File.separator
+						+ "chromedriver");
+			}
+			System.setProperty(ChromProp, targetChromedriver.getAbsolutePath());
+			driver = new ChromeDriver();
+		}
+	
 
 	@Test
 	public void testOpenGoogle() {
@@ -60,8 +93,10 @@ public class GoogleTesting extends TestNGBase {
 		
 	}
 	
-	@After
-	public void teardown() {
-		driver.quit();
-	}
+	@AfterMethod()
+	public void postTestCase() {
+		if (driver != null) {
+			driver.quit();
+		}
+	} 
 }
